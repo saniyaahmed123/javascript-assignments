fc15e19e81f890fdbc9d60e1644a9a277906daa7
saniyaahmed123 <saniya2012ahmed@gmail.com>
2025-12-23 23:13:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1. 'Greet visitor using alert
+alert('Welcome to my website!');
+
+// 2. Display message on web page
+alert("Error! Please enter a valid password.");
+
+
+// 3. Display message using line break
+alert('welcome to js land \n Happy coding!');
+
+
+// 4. Display messages in sequence
+alert('Welcome to JS Land...');
+alert('happy coding ! \n prevent this page from creating additional dialogs');
+
+// 5. Generate message through browser console
+console.log('Hello... I can run JS through my web browser console');
+
+// 6. Use alerts in any HTML/CSS project
+alert('page loaded successfully');
