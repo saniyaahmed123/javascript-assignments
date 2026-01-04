1742baff5f5146dc27238a1e4a9a7d4d41ba2235
saniyaahmed123 <saniya2012ahmed@gmail.com>
2026-01-05 00:06:43 +0500

    your message

---
+++ chap 14- 16/index.js
@@ -0,0 +1,18 @@
+// TASK 1 
+// declare nad initialize  a string array
+var  desserts = ['cake', 'cookie','pudding','icecream','tiramisu'];
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var boolean = [true,false];
+var mixed = ['tiramisu',6,true,90,'cake'];
+ var qualifications = ['SSC', 'HSC', 'BSC', 'BS','BCOM','MS','M. Phil.','PhD'];
+
+document.write('<h3>Qualifications:</h3>');
+var students = ['saniya','zainab','ayesha'];
+var scores = [320,280,240]
+var total_marks = 500;
+var percentage1 = (scores[0]/total_marks)*100;
+var percentage2 = (scores[1]/total_marks)*100;
+var percentage3 = (scores[2]/total_marks)*100;
+document.write('<h3>'+students[0]+'scored'+scores[0]+'percentage'+percentage1+'%</h3>');
+document.write('<h3>'+students[1]+'scored'+scores[1]+'percentage'+percentage2+'%</h3>');
+document.write('<h3>'+students[2]+'scored'+scores[2]+'percentage'+percentage3+'%</h3>');
